9e1db5a0a1022ae4f092c53b1b681d9680b9cb79
KimJY <kjy3501@naver.com>
2022-10-25 18:03:05 +0900

    업데이트

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,5 @@
-import {
-  createStore
-} from "vuex";
-import {
-  fetchAllApi,
-  fetchDetailInfo
-} from '../api/index'
-
+import { createStore } from "vuex";
+import { fetchAllApi, fetchDetailInfo } from "../api/index";
 
 export default createStore({
   //데이터
@@ -15,36 +9,31 @@ export default createStore({
   },
   //전체데이터호출
   actions: {
-    fetchMovieList({
-      commit
-    }) {
+    fetchMovieList({ commit }) {
       fetchAllApi()
-        .then(res => {
+        .then((res) => {
           //commit으로 결과전송
-          commit('MOVIE_LIST_INIT', res.data)
+          commit("MOVIE_LIST_INIT", res.data);
         })
-        .catch(err => console.log(err))
+        .catch((err) => console.log(err));
     },
 
     //상세데이터 호출
-    fetchMovieInfo({
-      commit
-    }, _id) {
+    fetchMovieInfo({ commit }, _id) {
       fetchDetailInfo(_id)
-        .then(res => {
-          commit('MOVIE_INFO', res.data)
+        .then((res) => {
+          commit("MOVIE_INFO", res.data);
         })
-        .catch(err => console.log(err))
-
-    }
+        .catch((err) => console.log(err));
+    },
   },
   mutations: {
     MOVIE_LIST_INIT(state, payload) {
-            state.movieList = payload;
+      state.movieList = payload;
     },
     MOVIE_INFO(state, payload) {
-            state.movieInfo = payload;
-    }
+      state.movieInfo = payload;
+    },
   },
 
   //데이터저장
@@ -55,6 +44,6 @@ export default createStore({
     },
     getMovieInfo(state) {
       return state.movieInfo;
-    }
-  }
+    },
+  },
 });
